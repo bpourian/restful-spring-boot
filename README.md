26da23a723d9caf46d7133869bee9b41423f4734
bpourian <b.pourian@hotmail.co.uk>
2018-09-25 12:02:03 +0100

    create controller and model

---
+++ README.md
@@ -0,0 +1,6 @@
+## RESTful Web Service
+
+Build in Java using Maven and Sping-Boot framework.
+
+Spring Boot is used to bootstrap and develop new Spring applications.
+
---
+++ src/main/java/com/pourian/rest/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.pourian.rest.controller;
+
+import com.pourian.rest.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting") // ensures that HTTP requests to /greeting are mapped to greeting() method
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+
+
+}
